a2f356970ce91305119bb4b3ebddea71e76ed0b7
hushibin <398994593@qq.com>
2023-09-28 08:37:18 +0800

    提交代码

--- app/src/main/java/com/handy/fetchbook/eventBus/EventVideoPlayOver.kt
+++
@@ -1,8 +0,0 @@
-package com.handy.fetchbook.eventBus
-
-/**
- * - Author: Charles
- * - Date: 2023/9/9
- * - Description:
- */
-class EventVideoPlayOver
--- app/src/main/java/com/handy/fetchbook/model/CaptchaGetOt.kt
+++
@@ -1,8 +0,0 @@
-package com.handy.fetchbook.model
-/**
- * Date:2020/4/30
- * author:wuyan
- */
-class CaptchaGetOt(
-    val captchaType: String//滑动拼图 blockPuzzle,文字点选 clickWord
-)
